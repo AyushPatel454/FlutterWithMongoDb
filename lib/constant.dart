8fda27ef9913268755361ea227057da4dc430e76
Aayush Patel <aayushpatel25190@gmail.com>
2023-01-30 15:57:06 +0530

    insert documents in mongodb collection

---
+++ lib/constant.dart
@@ -0,0 +1,2 @@
+const MONGO_URL = "mongodb+srv://ayush:2519@cluster0.9zui6gi.mongodb.net/test?retryWrites=true&w=majority";
+const COLLECTION_NAME = "users";
---
+++ lib/mongodb.dart
@@ -0,0 +1,26 @@
+import 'dart:developer';
+
+import 'package:flutter_mongodb/constant.dart';
+import 'package:mongo_dart/mongo_dart.dart';
+
+class MongoDatabase {
+  static connect() async {
+    var db = await Db.create(MONGO_URL); // create token of connection
+    await db.open(); // open connection
+    inspect(db); // show database setting. like whatever cluster setting
+    var status = db.serverStatus();
+    print(status);
+    var collection = db.collection(COLLECTION_NAME); // call the collection // this line use to delete , update, save anything on mongo database.
+    await collection.insertMany([{ // insert document (row) in collection. // insert multiple document (rows) // ---> collection.insertOne() ---> for insert only one document (rows)
+      "username":"amy",
+      "name":"Amy Sadhu",
+      "email":"amy@gmail.com"
+    },
+    {
+      "username":"amruta",
+      "name":"Amruta Sadhu",
+      "email":"amruta@gmail.com"
+    }]);
+    print(await collection.find().toList());
+  }
+}
